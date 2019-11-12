4ae33b7b92c151810235c948e1ad6b730fc8f768
Fredrika LINDH <frlindh@e1r1p1.42.fr>
2019-11-12 18:03:26 +0100

    home

--- math_vectors.c
+++ math_vectors.c
@@ -6,7 +6,7 @@
 /*   By: frlindh <frlindh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 15:32:00 by frlindh           #+#    #+#             */
-/*   Updated: 2019/11/12 15:55:27 by frlindh          ###   ########.fr       */
+/*   Updated: 2019/11/12 17:01:44 by frlindh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ float	dot(t_vector v1, t_vector v2)
 	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
 
-float	cross(t_vector v1, t_vector v2) // return float or vector ?
+t_vector	cross(t_vector v1, t_vector v2)
 {
 	return (vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z,
 						v1.x * v2.u - v1.y * v2.x));
